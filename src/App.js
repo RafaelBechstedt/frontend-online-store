8e8308df455b0f859b6d4401690acd95588b4569
Renan Fernandes <renzinestuods@gmail.com>
2022-08-05 15:11:24 -0300

    iniciando

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reacto
+          Learn React
         </a>
       </header>
     </div>
